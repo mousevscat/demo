5f0c19293655bf041b08f22365e6d5336fbf4ca1
mousevscat <75349593+mousevscat@users.noreply.github.com>
2024-01-15 23:30:13 +0800

    feat: bed and duck

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var moon = $moon
+@onready var duckSound = $AudioStreamPlayer
 
 func _ready():
 	moon.visible = false
@@ -12,3 +13,8 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.has_method("player"):
 		moon.visible = false
+
+
+func _on_duck_body_entered(body):
+	if body.has_method("player"):
+		duckSound.play()
